fdcab075c3036a748028d4698756af026993fa45
RomanVK <roman.kuzmin3333@gmail.com>
2021-07-03 17:32:36 +0300

    added to Controller validName

---
+++ src/ua/onlineua/homework/hw4_regex/Controller.java
@@ -0,0 +1,8 @@
+package ua.onlineua.homework.hw4_regex;
+
+public class Controller {
+    public boolean validateName() {
+        return true;
+    }
+
+}
---
+++ src/ua/onlineua/homework/hw4_regex/test/TestController.java
@@ -0,0 +1,14 @@
+package ua.onlineua.homework.hw4_regex.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+import ua.onlineua.homework.hw4_regex.Controller;
+
+public class TestController {
+    @Test
+    public void testValidateName(){
+        Controller controller = new Controller();
+        boolean isNameValid = controller.validateName();
+        Assert.assertTrue(isNameValid);
+    }
+}
