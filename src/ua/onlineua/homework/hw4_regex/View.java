5fa0d513ed5d2df1d210fd8b708ff34dc56ebc44
RomanVK <roman.kuzmin3333@gmail.com>
2021-07-03 22:47:52 +0300

    corrected View * corrected constants * deleted not used method concatenationString

--- src/ua/onlineua/homework/hw4_regex/View.java
+++ src/ua/onlineua/homework/hw4_regex/View.java
@@ -8,12 +8,12 @@ package ua.onlineua.homework.hw4_regex;
  */
 public class View {
     public static final String INTRO_MESSAGE =
-            "To add a new note to you notebook you must enter the following information about:";
-    public static final String INPUT_SURNAME = "Surname: ";
+            "To add a new note to you notebook you must input the following information:";
+    public static final String INPUT_SURNAME = "Input Surname: ";
     public static final String RULES_SURNAME =
             "For the surname can be used only alphabetical characters" +
                     "and the fallowing utility characters \''\' , \'.\',  \'-\'.";
-    public static final String INPUT_NICKNAME = "Nickname: ";
+    public static final String INPUT_NICKNAME = "Input Nickname: ";
     public static final String RULES_Nickname =
             "For the nickname can be used only letters and numbers. Nickname length must be from 6 to 30 characters.";
     public static final String SURNAME_IS_WRONG = "Surname is wrong.";
@@ -28,16 +28,4 @@ public class View {
         System.out.println(message);
     }
 
-    /**
-     * Prints a message from multiple String arguments
-     * @param message multiple Strings
-     * @return concatenation of multiple Strings
-     */
-    public String concatenationString (String ... message){
-        StringBuilder concatString = new StringBuilder();
-        for (String v : message){
-            concatString = concatString.append(v);
-        }
-        return new String(concatString);
-    }
 }
