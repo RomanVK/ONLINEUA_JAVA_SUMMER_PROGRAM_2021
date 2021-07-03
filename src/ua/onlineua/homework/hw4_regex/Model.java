0d76b2c0df0d09515d067e4c8966689a8055f316
RomanVK <roman.kuzmin3333@gmail.com>
2021-07-03 21:34:30 +0300

    Class Model is completed

--- src/ua/onlineua/homework/hw4_regex/Model.java
+++ src/ua/onlineua/homework/hw4_regex/Model.java
@@ -1,4 +1,33 @@
 package ua.onlineua.homework.hw4_regex;
 
+import java.util.ArrayList;
+/**
+ * Class {@code Model} is part of the MVC pattern.
+ * It contains business logic and data.
+ *
+ * @author  RomanVK
+ */
 public class Model {
+
+    ArrayList<Note> noteBook = new ArrayList<>();
+
+    public void addNote(String name, String nickname){
+        noteBook.add(new Note(name, nickname));
+    }
+
+}
+
+/**
+ * Class {@code Note} contains User's name and nickname .
+ *
+ * @author  RomanVK
+ */
+class Note{
+    String name;
+    String nickname;
+
+    Note(String name, String nickname){
+        this.name = name;
+        this.nickname = nickname;
+    }
 }
