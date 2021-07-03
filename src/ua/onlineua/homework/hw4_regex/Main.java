cb3d667fff092418de5bd085b9d23305f82340b5
RomanVK <roman.kuzmin3333@gmail.com>
2021-07-03 20:43:34 +0300

    Class Main is completed

--- src/ua/onlineua/homework/hw4_regex/Main.java
+++ src/ua/onlineua/homework/hw4_regex/Main.java
@@ -1,5 +1,15 @@
 package ua.onlineua.homework.hw4_regex;
 
+/**
+ * Class {@code Main} is program entry point.
+ *
+ *  @author  RomanVK
+ */
 public class Main {
-
+    public static void main(String[] args) {
+        // Initialization
+        Controller controller = new Controller(new Model(), new View());
+        // Run
+        controller.processUser();
+    }
 }
