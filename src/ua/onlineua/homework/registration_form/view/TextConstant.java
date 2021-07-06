8bdf35e8225094a7131ae5ca6805b40478502fa6
RomanVK <roman.kuzmin3333@gmail.com>
2021-07-06 22:33:05 +0300

    added interface TextConstant

---
+++ src/ua/onlineua/homework/registration_form/view/TextConstant.java
@@ -0,0 +1,13 @@
+package ua.onlineua.homework.registration_form.view;
+
+public interface TextConstant {
+    String INTRO_MESSAGE = "intro.message";
+    String INPUT_NAME = "input.first.name.data";
+    String RULES_NAME = "rules.first.name";
+    String INPUT_LOGIN = "input.login.data";
+    String RULES_LOGIN = "rules.login";
+    String NAME_IS_WRONG = "input.wrong.name";
+    String LOGIN_IS_WRONG = "input.wrong.login";
+    String TRY_AGAIN = "try.again";
+
+}
